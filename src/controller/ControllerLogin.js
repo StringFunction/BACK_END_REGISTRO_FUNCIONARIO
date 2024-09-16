c6f72108aeed8e8c950cdc6722ba78bdae0e4c70
cleciocarmais <121456337+cleciocarmais@users.noreply.github.com>
2024-09-15 23:48:22 -0300

    add verificao de token

--- src/controller/ControllerLogin.js
+++ src/controller/ControllerLogin.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const jwt = require("jsonwebtoken")
 const caminho = "./src/config/usuario.json"
 const caminha_lista_negra = "./src/config/lista_negraToken.json"
+const vericarToken = require("../middleware/verificarToken")
 
 //FUNCAO PARA GRAVA TOKEN INVALIDOS
 const  grava_token_na_lista_negra = async (infor) =>{
@@ -30,7 +31,7 @@ Router.post("/user", async (req, res) =>{
 
         if (!!result){
         const token =  jwt.sign(result,"ClecioBonitao", {expiresIn : "1h"})
-        return res.status(201).json({"token" : token})
+        return res.status(200).json({"token" : token})
         } else{
             return res.status(401).send({mensagem : "usuario nao encontrado"})
         }
@@ -38,17 +39,21 @@ Router.post("/user", async (req, res) =>{
     }catch (erro) {
         console.log(erro);
         
-        return res.status(401).send("erro interno")
+        return res.status(500).send("erro interno")
     }
 
     
 })
 //DESLOGAR E REGISTRA TOKEN 
-Router.post("/logaut",  async (req, res) =>{
+Router.post("/logaut", vericarToken, async (req, res) =>{
+    const registro = await JSON.parse(fs.readFileSync(caminha_lista_negra, 'utf-8'));
+    const index =  registro.lista_negra.findIndex(item => item == token)
+    if (index !== -1) return res.status(498).send({mensagem : "token na lista negra"})
+    
     const token = req.headers["x-access-token"]
     const response = await grava_token_na_lista_negra(token)
-    if (!!response) return res.status(201).send(response)
-    res.status(401).send({mensagem : "erro ao tenta grava token"})
+    if (!!response) return res.status(200).send(response)
+    res.status(500).send({mensagem : "erro ao tenta grava token"})
     
 
 })
--- src/middleware/verificarToken.js
+++ src/middleware/verificarToken.js
@@ -9,9 +9,9 @@ async function vericarToken(req, res, next) {
     const registro = await JSON.parse(fs.readFileSync(caminho, 'utf-8'));
     const index =  registro.lista_negra.findIndex(item => item == token)
 
-    if (index !== -1) return res.status(401).send({mensagem : "token na lista negra"})
+    if (index !== -1) return res.status(498).send({mensagem : "token na lista negra"})
     jwt.verify(token, "ClecioBonitao", (erro, decode) =>{
-        if (erro) return (res.status(401).send({mensagem : "token invalido"}))
+        if (erro) return (res.status(498).send({mensagem : "token invalido"}))
   
             req.matricula = decode.matricula
             req.nivel = decode.nivel
