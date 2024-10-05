ad39adc11fe4fe0a413894770b27111d5bce06fa
cleciocarmais <121456337+cleciocarmais@users.noreply.github.com>
2024-10-05 16:44:18 -0300

    nova versao com erro

--- src/controller/ControllerLogin.js
+++ src/controller/ControllerLogin.js
@@ -33,7 +33,7 @@ Router.post("", async (req, res) =>{
         if (!!result){
         console.log("Usuario autenticado " + result.nome + "\n" + "Matricula " + result.matricula);
         
-        const token =  jwt.sign(result,"ClecioBonitao", {expiresIn : "1h"})
+        const token =  jwt.sign(result,"ClecioBonitao", {expiresIn : "5m"})
         return res.status(200).send({"token" : token})
         } else{
             console.log("Usuario nao encontrado");
